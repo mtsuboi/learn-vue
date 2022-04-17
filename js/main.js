1ffa2d3a3d897abe188990e82054978cfc008cf1
mtsuboi <mtsuboied@gmail.com>
2022-04-18 07:55:56 +0900

    APIでリアルタイムサーチ

--- js/main.js
+++ js/main.js
@@ -1,22 +1,43 @@
 const app = Vue.createApp({
   data: () => ({
-    colors: [
-      { name: 'Red'},
-      { name: 'Green'},
-      { name: 'Blue'}
-    ]
+    items: null,
+    keyword: '',
+    message: ''
   }),
   watch: {
-    colors: {
-      handler: function(newValue, oldValue) {
-        console.log('Update!')
-      },
-      deep: true
+    keyword: function(newKeyword, oldKeyword) {
+      // console.log(newKseyword)
+      this.message = 'Waiting for you to stop tying...'
+      this.debouncedGetAnswer()
     }
   },
+  mounted :function() {
+    // this.keyword ='JavaScript'
+    // this.getAnswer()
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+  },
   methods: {
-    onClick: function(event) {
-      this.colors[1].name = 'White'
+    getAnswer: function() {
+      if(this.keyword === '') {
+        console.log('karamoji')
+        this.items = null
+        return
+      }
+
+      this.message = 'Loadging...'
+      const vm = this
+      const params = { page: 1, per_page: 20, query: this.keyword }
+      axios.get('https://qiita.com/api/v2/items', { params })
+        .then(function(response){
+          // console.log(response)
+          vm.items = response.data
+        })
+        .catch(function(error){
+          vm.message = 'Error!' + error
+        })
+        .finally(function() {
+          vm.message = ''
+        })
     }
   }
 })
