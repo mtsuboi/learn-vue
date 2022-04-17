642ad2d1de3e5733efcf32a4e9d3d49837b998ce
mtsuboi <mtsuboied@gmail.com>
2022-04-18 08:22:53 +0900

    v-bindで複数のクラスを切り替える

--- js/main.js
+++ js/main.js
@@ -1,44 +1,7 @@
 const app = Vue.createApp({
   data: () => ({
-    items: null,
-    keyword: '',
-    message: ''
+    isLarge: false,
+    hasError: true
   }),
-  watch: {
-    keyword: function(newKeyword, oldKeyword) {
-      // console.log(newKseyword)
-      this.message = 'Waiting for you to stop tying...'
-      this.debouncedGetAnswer()
-    }
-  },
-  mounted :function() {
-    // this.keyword ='JavaScript'
-    // this.getAnswer()
-    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
-  },
-  methods: {
-    getAnswer: function() {
-      if(this.keyword === '') {
-        console.log('karamoji')
-        this.items = null
-        return
-      }
-
-      this.message = 'Loadging...'
-      const vm = this
-      const params = { page: 1, per_page: 20, query: this.keyword }
-      axios.get('https://qiita.com/api/v2/items', { params })
-        .then(function(response){
-          // console.log(response)
-          vm.items = response.data
-        })
-        .catch(function(error){
-          vm.message = 'Error!' + error
-        })
-        .finally(function() {
-          vm.message = ''
-        })
-    }
-  }
 })
 app.mount('#app')
