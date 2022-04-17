9c103b5e870b333610c9a34041c6e8206f6ec246
mtsuboi <mtsuboied@gmail.com>
2022-04-17 17:51:14 +0900

    単位変換アプリ

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const app = Vue.createApp({
   data: () => ({
-    message: 'Hello Vue.js!'
+    km: 0,
+    m: 0
   }),
   watch: {
-    message: function(newValue, oldValue) {
-      console.log('new: %s old: %s', newValue, oldValue)
+    km: function(value) {
+      console.log(value)
+      this.km = value
+      this.m = value * 1000
+    },
+    m:function(value) {
+      this.km = value / 1000
+      this.m = value
     }
   }
 })
