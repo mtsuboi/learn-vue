9add8b476f17c91157a59f86d6d444e595c148a6
mtsuboi <mtsuboied@gmail.com>
2022-04-17 16:27:03 +0900

    v-preディレクティブ

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 const app = Vue.createApp({
   data: () => ({
     message: 'Hello Vue.js!'
-  }),
-  methods: {
-    clickHandler: function(event) {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
+  })
 })
 app.mount('#app')
