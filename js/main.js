adfdddd0b1a00c7d794a41174cc523660c471624
mtsuboi <mtsuboied@gmail.com>
2022-04-17 17:24:39 +0900

    computed vs method

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ const app = Vue.createApp({
     reversedMessage: function() {
       return this.message.split('').reverse().join('')
     }
+  },
+  methods: {
+    reversedMessageMethod: function() {
+      return this.message.split('').reverse().join('')
+    }
   }
 })
 app.mount('#app')
