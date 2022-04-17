03ca1baf3c5f63d043a3f3f1804043d20670fcac
mtsuboi <mtsuboied@gmail.com>
2022-04-17 17:30:39 +0900

    getter setter

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
   data: () => ({
-    message: 'Hello Vue.js!'
+    basePrice: 100
   }),
   computed: {
-    reversedMessage: function() {
-      return this.message.split('').reverse().join('')
-    }
-  },
-  methods: {
-    reversedMessageMethod: function() {
-      return this.message.split('').reverse().join('')
+    taxIncludedPrice: {
+      get: function() {
+        return this.basePrice * 1.1
+      },
+      set: function(value) {
+        this.basePrice = value / 1.1
+      }
     }
   }
 })
