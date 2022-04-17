14b18ad1a62a50c466dd3c3b3f92def2b123c601
mtsuboi <mtsuboied@gmail.com>
2022-04-17 17:38:50 +0900

    computedとmethodsのキャッシュ

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@ const app = Vue.createApp({
     basePrice: 100
   }),
   computed: {
-    taxIncludedPrice: {
-      get: function() {
-        return this.basePrice * 1.1
-      },
-      set: function(value) {
-        this.basePrice = value / 1.1
-      }
+    computedNumber: function() {
+      console.log('computed!')
+      return Math.random()
+    }
+  },
+  methods: {
+    methodsNumber: function() {
+      console.log('methods!')
+      return Math.random()
     }
   }
 })
