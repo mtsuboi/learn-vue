32c7a2accdcb14008111f3ef9bd5f4a2f4914dd3
mtsuboi <mtsuboied@gmail.com>
2022-04-19 07:45:39 +0900

    イベントハンドラーで引数を使う

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const app = Vue.createApp({
   data: () => ({
-    counter: 0
+    message: ''
   }),
   methods: {
-    clickHandler: function() {
-      this.counter++
+    clickHandler: function(message) {
+      // console.log(message)
+      this.message = message
     }
   }
 })
